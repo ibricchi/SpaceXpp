70e3fd86cf7b37d0e194b8ddeefde3e1f90d5c70
nicholas <kontakt@nicholaspfaff.ch>
2021-05-16 21:38:41 +0100

    Add poc code for esp32 MQTT communication with Mosquitto Broker running on AWS Free Tier
    This includes connecting to WiFi

---
+++ control/poc/mqtt/esp32/main.c
@@ -0,0 +1,236 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+
+static const char *MQTT_tag = "MQTT";
+static const char *Wifi_tag = "wifi station";
+
+#define ESP_WIFI_SSID      "<SSID (WiFi name)>"
+#define ESP_WIFI_PASS      "<WiFi password>"
+#define ESP_MAXIMUM_RETRY  10
+
+#define MQTT_BROKER_URI "mqtt://18.130.239.157" // Mosquitto MQTT broker hosted on AWS Free Tier
+
+// FreeRTOS event group to signal when we are connected
+static EventGroupHandle_t s_wifi_event_group;
+
+/* 
+    The event group allows multiple bits for each event, but we only care about two events:
+    - we are connected to the AP with an IP
+    - we failed to connect after the maximum amount of retries 
+*/
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+static int s_retry_num = 0;
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        ESP_ERROR_CHECK(esp_wifi_connect());
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < ESP_MAXIMUM_RETRY) {
+            ESP_ERROR_CHECK(esp_wifi_connect());
+            s_retry_num++;
+            ESP_LOGI(Wifi_tag, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(Wifi_tag,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(Wifi_tag, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+static void wifi_init_sta()
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL, &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = ESP_WIFI_SSID,
+            .password = ESP_WIFI_PASS,
+	        .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+            .pmf_cfg = {
+                .capable = true,
+                .required = false
+            },
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(Wifi_tag, "wifi_init_sta finished.");
+
+    /* 
+        Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+        number of re-tries (WIFI_FAIL_BIT). The bits are set by wifi_event_handler()
+    */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT | WIFI_FAIL_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
+
+    // xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually happened.
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(Wifi_tag, "connected to ap SSID:%s, password:%s", ESP_WIFI_SSID, ESP_WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(Wifi_tag, "Failed to connect to SSID:%s, password:%s", ESP_WIFI_SSID, ESP_WIFI_PASS);
+    } else {
+        ESP_LOGE(Wifi_tag, "UNEXPECTED EVENT");
+    }
+
+    // The event will not be processed after unregister
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    vEventGroupDelete(s_wifi_event_group);
+}
+
+// Probably better to only handle necessary events in production version
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(MQTT_tag, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+
+    switch ((esp_mqtt_event_id_t)event_id) {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(MQTT_tag, "MQTT_EVENT_CONNECTED");
+
+        msg_id = esp_mqtt_client_subscribe(client, "/test/test1", 0);
+        ESP_LOGI(MQTT_tag, "sent subscribe successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_subscribe(client, "/test/test2", 2);
+        ESP_LOGI(MQTT_tag, "sent subscribe successful, msg_id=%d", msg_id);
+
+        break;
+
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(MQTT_tag, "MQTT_EVENT_DISCONNECTED");
+
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(MQTT_tag, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+
+        break;
+
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(MQTT_tag, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        
+        break;
+
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(MQTT_tag, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        
+        break;
+
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(MQTT_tag, "MQTT_EVENT_DATA");
+        
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        
+        break;
+
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(MQTT_tag, "MQTT_EVENT_ERROR");
+
+        break;
+
+    default:
+        ESP_LOGI(MQTT_tag, "Other event id:%d", event->event_id);
+        
+        break;
+    }
+}
+
+static esp_mqtt_client_handle_t mqtt_init()
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .uri = MQTT_BROKER_URI,
+        // .disable_keepalive = true,
+        // .cert_pem = // Used for SSL
+    };
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+
+    ESP_ERROR_CHECK(esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client));
+    ESP_ERROR_CHECK(esp_mqtt_client_start(client));
+
+    return client;
+}
+
+static void mqtt_test_task(void *arg)
+{
+    esp_mqtt_client_handle_t client = (esp_mqtt_client_handle_t)arg;
+
+    int counter = 0;
+    int msg_id;
+    while (1) {
+        char data[20];
+        sprintf(data, "Counter is %d", counter);
+        if (counter % 2 == 0) {
+            msg_id = esp_mqtt_client_publish(client, "/test/test1", data, 0, 0, 0);
+        } else {
+            msg_id = esp_mqtt_client_publish(client, "/test/test2", data, 0, 2, 0);
+        }
+        ESP_LOGI(MQTT_tag, "sent publish successful, msg_id=%d", msg_id);
+
+        ++counter;
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void app_main()
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    wifi_init_sta();
+
+    esp_mqtt_client_handle_t client = mqtt_init();;
+
+    xTaskCreate(mqtt_test_task, "mqtt_test_task", 2000, (void*)client, 1, NULL);
+}
