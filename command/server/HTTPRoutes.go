c15d38f85c7724ad9e3fc8e9187f87ef77204878
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2021-05-20 10:04:10 +0100

    fixing cors issue

--- command/server/HTTPRoutes.go
+++ command/server/HTTPRoutes.go
@@ -2,11 +2,29 @@ package server
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 )
 
+func allowOriginFunc(r *http.Request, origin string) bool {
+	// Allow all without checking origin
+	return true
+}
+
 func (h *HttpServer) routes(ctx context.Context) error {
+
+	// General middleware
+	h.router.Use(cors.Handler(cors.Options{
+		AllowOriginFunc:  allowOriginFunc,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"X-PINGOTHER", "Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+	}))
+	h.router.Use(middleware.Logger)
 
 	// Public routes
 	h.router.Group(func(r chi.Router) {
