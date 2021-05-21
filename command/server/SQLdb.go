8c44c02de7591bcd89587e62d0c69037970cbab1
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2021-05-21 14:00:17 +0100

    db testing

--- command/server/SQLdb.go
+++ command/server/SQLdb.go
@@ -17,6 +17,18 @@ func (s *SQLiteDB) createTable(ctx context.Context) error {
 	fmt.Println("Creating summary table")
 	statement, _ := s.db.Prepare("CREATE TABLE IF NOT EXISTS summary (id INTEGER PRIMARY KEY, name TEXT)")
 	statement.Exec()
+	statement, _ = s.db.Prepare("INSERT INTO summary (name) VALUES (?)")
+	statement.Exec("Brad")
+
+	fmt.Println("data in db, now querying")
+
+	rows, _ := s.db.Query("SELECT id, name FROM people")
+	var id int
+	var name string
+	for rows.Next() {
+		rows.Scan(&id, &name)
+		fmt.Println(strconv.Itoa(id) + ": " + name)
+	}
 
 	return nil
 }
