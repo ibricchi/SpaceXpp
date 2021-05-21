b08a0554e9347a5c6c2a508fd490cc371aa13097
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2021-05-21 15:19:27 +0100

    db testing

--- command/server/SQLdb.go
+++ command/server/SQLdb.go
@@ -25,7 +25,7 @@ func (s *SQLiteDB) insertData(status bool, battery int) error {
 	statement, _ := s.db.Prepare("CREATE TABLE IF NOT EXISTS summary (id INTEGER PRIMARY KEY, battery INTEGER)")
 	statement.Exec()
 	statement, _ = s.db.Prepare("INSERT INTO summary (battery) VALUES (?)")
-	statement.Exec(10)
+	statement.Exec(&battery)
 
 	rows, _ := s.db.Query("SELECT id, battery FROM summary")
 	var id int
