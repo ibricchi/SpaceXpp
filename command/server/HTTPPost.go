9b9b3e64a28fec85b22aa7e48e256d5f8e37ca80
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2021-05-23 14:39:16 +0100

    POSTS for drive

--- command/server/HTTPPost.go
+++ command/server/HTTPPost.go
@@ -25,3 +25,35 @@ func (h *HttpServer) speed(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(t)
 
 }
+
+func (h *HttpServer) driveD(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+
+	var t int
+	err := decoder.Decode(&t)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Send data to hardware
+	fmt.Println(t)
+
+}
+
+func (h *HttpServer) driveA(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+
+	var t int
+	err := decoder.Decode(&t)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Send data to hardware
+	fmt.Println(t)
+
+}
