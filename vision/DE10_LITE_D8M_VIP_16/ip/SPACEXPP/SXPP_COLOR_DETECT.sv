2c63b4af6597f49dc5d9c460100d495789f4d31d
IBricchi <ignacio.bricchi@gmail.com>
2021-05-26 12:32:10 +0100

    add rgb2hsv converter

--- vision/DE10_LITE_D8M_VIP_16/ip/SPACEXPP/SXPP_COLOR_DETECT.sv
+++ vision/DE10_LITE_D8M_VIP_16/ip/SPACEXPP/SXPP_COLOR_DETECT.sv
@@ -1,25 +1,28 @@
 module SXPP_COLOR_DETECT(
-	clk,
-	reset_n,
+	input clk,
+	input reset_n,
 	
-	r, g, b,
+	input[7:0] r,
+    input[7:0] g,
+    input[7:0] b,
 	
-	is_red, is_blue, is_green
+	output is_red,
+    output is_blue,
+    output is_green
 );
 
-input clk, reset_n;
-input [7:0] r, g, b;
-output is_red, is_blue, is_green;
+parameter max_dist = 40000;
 
-integer max_dist = 40000;
+logic [31:0] red_dist, green_dist, blue_dist;
 
-wire [31:0] red_dist, green_dist, blue_dist;
-assign red_dist =   (r - 255)*(r-255) + g*g + b*b;
-assign green_dist = (g - 255)*(g-255) + b*b + r*r;
-assign blue_dist =  (b - 255)*(b-255) + r*r + g*g;
-
-assign is_red   = red_dist < max_dist;
-assign is_green = green_dist < max_dist;
-assign is_blue  = blue_dist < max_dist;
+always_comb begin
+    red_dist =   (r - 255)*(r-255) + g*g + b*b;
+    green_dist = (g - 255)*(g-255) + b*b + r*r;
+    blue_dist =  (b - 255)*(b-255) + r*r + g*g;
+    
+    is_red   = red_dist < max_dist;
+    is_green = green_dist < max_dist;
+    is_blue  = blue_dist < max_dist;
+end
 
 endmodule
