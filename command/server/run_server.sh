d2ee173f18e54632c895a3b86e09fa279fddc31f
nicholas-p1 <nicholas.pfaff19@imperial.ac.uk>
2021-05-26 15:48:55 +0100

    Change HTTP server to use TLS

--- command/server/run_server.sh
+++ command/server/run_server.sh
@@ -3,8 +3,10 @@
 set -eou pipefail
 
 HTTP_PORT="${1:-3000}"
-MQTT_BROKER_URL="${2:-ssl://18.130.239.157:8883}"
-DB_NAME="${3:-serverDB.db}"
+HTTP_TLS_CERT_FILE="${2:-cert/server.crt}"
+HTTP_TLS_KEY_FILE="${3:-cert/server.key}"
+MQTT_BROKER_URL="${4:-ssl://18.130.239.157:8883}"
+DB_NAME="${5:-serverDB.db}"
 
 mkdir -p bin db
 
