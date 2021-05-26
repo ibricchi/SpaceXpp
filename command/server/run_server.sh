4286692f18555be5a92d497615e610792559f366
nicholas-p1 <nicholas.pfaff19@imperial.ac.uk>
2021-05-26 14:17:56 +0100

    Update run_server.sh

--- command/server/run_server.sh
+++ command/server/run_server.sh
@@ -2,8 +2,12 @@
 
 set -eou pipefail
 
-MQTT_BROKER_URL="${1:-ssl://18.130.239.157:8883}"
+HTTP_PORT="${1:-3000}"
+MQTT_BROKER_URL="${2:-ssl://18.130.239.157:8883}"
+DB_NAME="${3:-serverDB.db}"
+
+mkdir -p bin db
 
 make server
 
-./bin/server -mqttBrokerURL ${MQTT_BROKER_URL}
+./bin/server -httpPort ${HTTP_PORT} -mqttBrokerURL ${MQTT_BROKER_URL} -db ${DB_NAME}
