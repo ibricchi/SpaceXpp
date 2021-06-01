1b706f1672dfc85b978a6433f29b15d7582cdefe
nicholas <kontakt@nicholaspfaff.ch>
2021-06-01 10:29:10 +0100

    Implement navigation feedback logic and add tasks for simulating the drive and vision module

--- control/spaceXpp_rover_controller/include/drive.h
+++ control/spaceXpp_rover_controller/include/drive.h
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
@@ -5,16 +6,25 @@
 #ifndef DRIVE_H
 #define DRIVE_H
 
+// tile with in cm
+#define MAP_TILE_WIDTH (10)
+
 #define MAX_DRIVE_INSTRUCTION_SEQUENCE_LENGTH (50)
 #define DRIVE_INSTRUCTION_DELIMITER ":"
 
 // UART data encoding
 typedef struct DriveEncoding{
     const char* forward;
+    const char* backward;
     const char* turnRight;
     const char* turnLeft;
+    const char* stop;
 } DriveEncoding;
 
 void drive_init();
+
+void flush_drive_instruction_queue();
+
+void drive_backwards_to_last_valid_tile(int distanceDrivenOfLastInstruction);
 
 #endif
