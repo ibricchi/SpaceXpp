1735fc1e1b0b7d9a431a87fcdad3fed1397f78a4
nicholas <kontakt@nicholaspfaff.ch>
2021-06-01 10:29:10 +0100

    Implement navigation feedback logic and add tasks for simulating the drive and vision module

--- control/spaceXpp_rover_controller/src/drive.c
+++ control/spaceXpp_rover_controller/src/drive.c
@@ -2,7 +2,11 @@
 
 static const char *DRIVE_tag = "DRIVE";
 
-const DriveEncoding driveEncoding = {"F", "R", "L"};
+const DriveEncoding driveEncoding = {"F", "B", "R", "L", "S"};
+
+// Used for calculating backward distance after forced stop
+char* currentDriveInstruction = "";
+int currentDriveInstructionValue = 0;
 
 // Global FIFO queue used for drive instruction sequence
 QueueHandle_t driveInstructionQueue;
@@ -15,4 +19,29 @@ void drive_init() {
     }
 
     ESP_LOGI(DRIVE_tag, "Drive setup completed");
+}
+
+void flush_drive_instruction_queue() {
+    char* data = (char*) malloc(32*8);
+
+    for (int i=0; i<MAX_DRIVE_INSTRUCTION_SEQUENCE_LENGTH; i++) {
+        xQueueReceive(driveInstructionQueue, data, (TickType_t)0);
+    }
+
+    free(data);
+
+    ESP_LOGI(DRIVE_tag, "Finished flashing drive instruction queue");
+}
+
+void drive_backwards_to_last_valid_tile(int distanceDrivenOfLastInstruction) {
+    if (!(strcmp(currentDriveInstruction, driveEncoding.forward) == 0)) {
+        ESP_LOGE(DRIVE_tag, "Current instruction not forward, can't calculate distance to drive backwards: %s", currentDriveInstruction);
+        return;
+    }
+
+    int backwardDistance = distanceDrivenOfLastInstruction % MAP_TILE_WIDTH;
+
+    char encodedData[32];
+    sprintf(encodedData, "%s%s%d", driveEncoding.backward, DRIVE_INSTRUCTION_DELIMITER, backwardDistance);
+    xQueueSend(driveInstructionQueue, (void*)encodedData, (TickType_t)0);
 }
--- control/spaceXpp_rover_controller/src/uart.c
+++ control/spaceXpp_rover_controller/src/uart.c
@@ -4,6 +4,8 @@ static const char *UART_tag = "UART";
 
 extern const DriveEncoding driveEncoding;
 extern QueueHandle_t driveInstructionQueue;
+extern char* currentDriveInstruction;
+extern int currentDriveInstructionValue;
 
 void drive_uart_init()
 {
@@ -37,7 +39,7 @@ void drive_uart_task(void *arg)
 {
     static bool driveIsReady = false;
 
-    uint8_t* rx_data = (uint8_t*) malloc(DRIVE_BUFFER_SIZE+1);
+    uint8_t* rx_data = (uint8_t*) malloc(DRIVE_BUFFER_SIZE);
     char* queue_data = (char*) malloc(32*8);
 
     while (1) {
@@ -46,24 +48,41 @@ void drive_uart_task(void *arg)
             rx_data[rxBytes] = 0; // End of received string
             printf("UART data from drive: %s\n", (char*)rx_data);
 
-            char quickInstruction = rx_data[0];
-            if (quickInstruction == 'R') { // Drive is ready for next instruction
-                driveIsReady = true;
+            const char instruction = rx_data[strlen((char*)rx_data)-1];
+            rx_data[strlen((char*)rx_data)-1] = '\0';
+            switch (instruction) {
+                case 'R': // Drive is ready for next instruction
+                    driveIsReady = true;
+                    break;
+                case 'S': // Stop: Distance of last forward instruction driven
+                    drive_backwards_to_last_valid_tile(atoi((char*)rx_data));
+                    publish_drive_instruction_to_server("SD", (char*)rx_data); // second part of stop instruction for server
+                    break;
+                default:
+                    ESP_LOGE(UART_tag, "Instruction from drive not valid: %d", instruction);
+                    break;
             }
         }
 
         // Don't block if queue is empty, only send next instruction if drive is ready
         if (driveIsReady && xQueueReceive(driveInstructionQueue, queue_data, (TickType_t)0)) {
+            // Check if end of instruction sequence signal (used by server for updating map)
+            if (strcmp(queue_data, "X") == 0) {
+                publish_drive_instruction_to_server(queue_data, "0");
+            }
+
             // Decode drive instruction data
             char* instruction = strtok(queue_data, DRIVE_INSTRUCTION_DELIMITER);
             char* value = strtok(NULL, DRIVE_INSTRUCTION_DELIMITER);
 
             char* encodedInstruction = "";
-            if (strcmp(instruction, "F") || strcmp(instruction, "forward")) {
+            if (strcmp(instruction, "F") == 0 || strcmp(instruction, "forward") == 0) {
                 encodedInstruction = driveEncoding.forward;
-            } else if (strcmp(instruction, "R") || strcmp(instruction, "turnRight")) {
+            } else if (strcmp(instruction, "B") == 0 || strcmp(instruction, "backward") == 0) {
+                encodedInstruction = driveEncoding.backward;
+            } else if (strcmp(instruction, "R") == 0 || strcmp(instruction, "turnRight") == 0) {
                 encodedInstruction = driveEncoding.turnRight;
-            } else if (strcmp(instruction, "L") || strcmp(instruction, "turnLeft")) {
+            } else if (strcmp(instruction, "L") == 0 || strcmp(instruction, "turnLeft") == 0) {
                 encodedInstruction = driveEncoding.turnLeft;
             } else {
                 ESP_LOGE(UART_tag, "Unknown drive instruction: %s", instruction);
@@ -72,6 +91,107 @@ void drive_uart_task(void *arg)
             // Send instruction to drive
             send_drive_uart_data(encodedInstruction, value);
             driveIsReady = false;
+
+            // Send instruction to server for updating map
+            publish_drive_instruction_to_server(encodedInstruction, value);
+
+            // Update current instruction
+            currentDriveInstruction = encodedInstruction;
+            currentDriveInstructionValue = atoi(value);
+        }
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    free(rx_data);
+    free(queue_data);
+}
+
+void drive_uart_task_simulated(void *arg) {
+    static bool driveIsReady = false;
+
+    uint8_t* rx_data = (uint8_t*) malloc(DRIVE_BUFFER_SIZE);
+    char* queue_data = (char*) malloc(32*8);
+
+    while (1) {
+        // Clear rx_data
+        memset(rx_data, 0, DRIVE_BUFFER_SIZE);
+
+        if (strcmp(currentDriveInstruction, driveEncoding.stop) == 0) {
+            int randomDistance = esp_random() % 60 + 1;
+            sprintf((char*)rx_data, "%d%s", randomDistance, "S");
+        } else {
+            switch (esp_random() % 3) {
+                case 0: // Ready for next instruction
+                    strcpy((char*)rx_data, "R");
+                    break;
+                case 1: // Not yet ready for next instruction => No data from drive
+                case 2:
+                    break;
+                default:
+                    ESP_LOGE(UART_tag, "Drive simulation: Random number not in allowed range.");
+                    break;
+            }
+        }
+
+        // simulated receiving from UART
+        int rxBytes = strlen((char*)rx_data);
+        if (rxBytes > 0) {
+            rx_data[rxBytes] = '\0'; // End of received string
+
+            ESP_LOGI(UART_tag, "Simulated data from drive: %s", (char*)rx_data);
+
+            const char instruction = rx_data[strlen((char*)rx_data)-1];
+            rx_data[strlen((char*)rx_data)-1] = '\0';
+            switch (instruction) {
+                case 'R': // Drive is ready for next instruction
+                    driveIsReady = true;
+                    break;
+                case 'S': // Stop: Distance of last forward instruction driven
+                    drive_backwards_to_last_valid_tile(atoi((char*)rx_data));
+                    publish_drive_instruction_to_server("SD", (char*)rx_data); // second part of stop instruction for server
+                    currentDriveInstruction = ""; // needed in simulation to prevent drive from sending stop distance multiple times
+                    break;
+                default:
+                    ESP_LOGE(UART_tag, "Simulated instruction from drive not valid: %d", instruction);
+                    break;
+            }
+        }
+
+        // Don't block if queue is empty, only send next instruction if drive is ready
+        if (driveIsReady && xQueueReceive(driveInstructionQueue, queue_data, (TickType_t)0)) {
+            // Check if end of instruction sequence signal (used by server for updating map)
+            if (strcmp(queue_data, "X") == 0) {
+                publish_drive_instruction_to_server(queue_data, "0");
+            }
+            
+            // Decode drive instruction data
+            char* instruction = strtok(queue_data, DRIVE_INSTRUCTION_DELIMITER);
+            char* value = strtok(NULL, DRIVE_INSTRUCTION_DELIMITER);
+
+            char* encodedInstruction = "";
+            if (strcmp(instruction, "F") == 0 || strcmp(instruction, "forward") == 0) {
+                encodedInstruction = driveEncoding.forward;
+            } else if (strcmp(instruction, "B") == 0 || strcmp(instruction, "backward") == 0) {
+                encodedInstruction = driveEncoding.backward;
+            } else if (strcmp(instruction, "R") == 0 || strcmp(instruction, "turnRight") == 0) {
+                encodedInstruction = driveEncoding.turnRight;
+            } else if (strcmp(instruction, "L") == 0 || strcmp(instruction, "turnLeft") == 0) {
+                encodedInstruction = driveEncoding.turnLeft;
+            } else {
+                ESP_LOGE(UART_tag, "Unknown simulated drive instruction: %s", instruction);
+            }
+
+            // Send instruction to drive
+            send_drive_uart_data(encodedInstruction, value);
+            driveIsReady = false;
+
+            // Send instruction to server for updating map
+            publish_drive_instruction_to_server(encodedInstruction, value);
+
+            // Update current instruction
+            currentDriveInstruction = encodedInstruction;
+            currentDriveInstructionValue = atoi(value);
         }
 
         vTaskDelay(1000 / portTICK_PERIOD_MS);
