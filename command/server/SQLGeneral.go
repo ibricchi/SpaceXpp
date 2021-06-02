e146cfaa41a35a4e2f091f7046b7d883d35f1402
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2021-06-02 15:26:24 +0100

    db

--- command/server/SQLGeneral.go
+++ command/server/SQLGeneral.go
@@ -43,7 +43,7 @@ func (s *SQLiteDB) migrate(ctx context.Context) error {
 	if _, err := s.db.ExecContext(ctx, `
 			CREATE TABLE IF NOT EXISTS maps (
 				mapid INTEGER NOT NULL PRIMARY KEY,
-				name STRING,
+				name STRING
 			)
 		`); err != nil {
 		return fmt.Errorf("sqlite failed to create summary table: %w", err)
