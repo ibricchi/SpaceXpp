f08c2de26650f74bc36c6bbef1fc8b0752599d79
IBricchi <ignacio.bricchi@gmail.com>
2021-06-08 20:33:14 +0100

    Add serial connector

---
+++ energy/comm/ad.py
@@ -0,0 +1,11 @@
+import serial
+import time
+ser = serial.Serial('COM3', 9600, timeout=0)
+ 
+while 1:
+    try:
+        print ser.readline()
+        time.sleep(1)
+    except ser.SerialTimeoutException:
+        print('Data could not be read')
+        time.sleep(1)
