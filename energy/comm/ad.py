ac93a5cd7262dbfa2477a52f649b4071d76e657f
IBricchi <ignacio.bricchi@gmail.com>
2021-06-08 20:39:20 +0100

    Fix python type

--- energy/comm/ad.py
+++ energy/comm/ad.py
@@ -4,7 +4,7 @@ ser = serial.Serial('COM3', 9600, timeout=0)
  
 while 1:
     try:
-        print ser.readline()
+        print(ser.readline())
         time.sleep(1)
     except ser.SerialTimeoutException:
         print('Data could not be read')
