9dc6fe7fa074159bae25ca07d0f827ca6f60aecf
Benjamin <bramhorst27@gmail.com>
2021-06-11 12:37:43 +0100

    Added speed control

--- drive/move_hl.cpp
+++ drive/move_hl.cpp
@@ -1,7 +1,7 @@
 #include "move_hl.h"
 
 // Sets up the drive functions
-void driveInit(){
+void driveInit() {
   motorSetup();
 }
 
@@ -24,41 +24,61 @@ bool moveBackwardForTime(unsigned long t, unsigned long currentInstructionTime) 
 }
 
 // Causes the rover to move forward for a distance d [cm] and returns true after moving the given distance
-bool moveForwardForDistance(float d, float initialY, float currentY) {
+movement moveForwardForDistance(float d, float initialY, float currentY) {
+  movement ans;
   if (currentY < (initialY + d)) {
     forwards();
-    return false;
+    ans.e = abs(initialY + d - currentY);
+    ans.done = false;
+  }else{
+    ans.e = 0.0;
+    ans.done = true;
   }
-  return true;
+  return ans;
 }
 
 // Causes the rover to move backward for a distance d [cm] and returns true after moving the given distance
-bool moveBackwardForDistance(float d, float initialY, float currentY) {
+movement moveBackwardForDistance(float d, float initialY, float currentY) {
+  movement ans;
   if (currentY > (initialY - d)) {
     backwards();
-    return false;
+    ans.e = abs(currentY - initialY + d);
+    ans.done = false;
+  }else{
+    ans.e = 0.0;
+    ans.done = true;
   }
-  return true;
+  return ans;
 }
 
 // Rotates clockwise 90 degrees
-bool turnRight(float currentX, float currentY, float initialX, float initialY) {
+movement turnRight(float currentX, float currentY, float initialX, float initialY) {
   float currentHypotenuse = (currentX - initialX) * (currentX - initialX) + (currentY - initialY) * (currentY - initialY);
-  float desiredHypotenuse = 2 * distanceFromRoverCentreToSensor * distanceFromRoverCentreToSensor;
+  float desiredHypotenuse = 2 * distanceFromRoverCentreToSensor * distanceFromRoverCentreToSensor;  // distance is 16.2 [cm]
+  movement ans;
   if (currentHypotenuse < desiredHypotenuse) {
     anticlockwise();
-    return false;
+    ans.e = abs(desiredHypotenuse - currentHypotenuse);
+    ans.done = false;
+  } else {
+    ans.e = 0.0;
+    ans.done = true;
   }
-  return true;
+  return ans;
 }
 
 // Rotates anticlockwise 90 degrees
-bool turnLeft(float currentX, float currentY, float initialX, float initialY) {
+movement turnLeft(float currentX, float currentY, float initialX, float initialY) {
   float currentHypotenuse = (currentX - initialX) * (currentX - initialX) + (currentY - initialY) * (currentY - initialY);
   float desiredHypotenuse = 2 * distanceFromRoverCentreToSensor * distanceFromRoverCentreToSensor;
+  movement ans;
   if (currentHypotenuse < desiredHypotenuse) {
     clockwise();
-    return false;
+    ans.e = abs(desiredHypotenuse - currentHypotenuse);
+    ans.done = false;
+  } else {
+    ans.e = 0.0;
+    ans.done = true;
   }
-  return true;
+  return ans;
 }
