f70fe4d83e33187d08cd219f8af62a0ee60b0344
Benjamin <bramhorst27@gmail.com>
2021-06-14 14:32:01 +0100

    Added copyright for drive module

--- drive/optical_flow.cpp
+++ drive/optical_flow.cpp
@@ -1,3 +1,13 @@
+/*
+  Program written by Benjamin Ramhorst (br319@ic.ac.uk) during May-June 2021
+  as part of SpaceX++ team for the Summer term design project at Imperial College London
+  SMPS code originally written by Yue Zhu (yue.zhu18@imperial.ac.uk) in July 2020 and further
+  modfied by Benjamin Ramhorst to fit the requirements of the Mars Rover
+  Teamplete code for optical flow and movement functions taken from MS Teams for the Summer Electronics Design Project 2 2021
+  and further modified by Benjamin Ramhorst to fit the project requirements
+  UART code written by Nicholas Pfaff and modified by Benjamin Ramhorst
+*/
+
 #include "optical_flow.h"
 
 // Default constructor
